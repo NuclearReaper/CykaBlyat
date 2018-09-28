8decce54bcd0f9b50c97cc5448e43cd66d83cb3c
NuclearReaper <43674661+NuclearReaper@users.noreply.github.com>
2018-09-28 18:37:03 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDk1MjIzODQ0NjUxNTMyMjkw.Do-8tQ.x98zhQoy78rKAksStS-bRGRZOqE);
+client.login(process.env.BOT_TOKEN);
